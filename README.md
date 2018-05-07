30690eb7dd4e8a28dd5a72fd730f4ac9dcc7552f
Prashanth <xavier211192@gmail.com>
2018-05-08 00:12:25 +0200

    Next

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Sample Project
+Speed Test
 ----------------
 
-This is a sample project to demonstrate git.  All files except this one are blank.
+This is a simple program that allows you to create a number of files of given size
+The result gives you the time taken for the process.
+Run this program on different USB devices to see the difference in copy speed.
--- speed_test.py
+++ speed_test.py
@@ -1,9 +1,9 @@
 
 import os
 import time
-filesize = int(raw_input("Enter Size of file to copy(1,2,5) in MB "))
-numberofFiles = int(raw_input("Enter number of files you would like to make"))
-#print filesize
+filesize = int(input("Enter Size of file to copy(1,2,5) in MB "))
+numberofFiles = int(input("Enter number of files you would like to make"))
+
 bsize = filesize *1024000 #convert mb to bytes( each character is one byte)
 
 
@@ -11,8 +11,8 @@ bsize = filesize *1024000 #convert mb to bytes( each character is one byte)
 '''This code below creates empty string list and appends letters into it until the desired size is reached'''
 a=[]  #empty list
 for i in range(bsize):    
-    a.append('KX')
-    #print a[i]
+    a.append('X')
+    
 
 
 ''' This code starts recording time and for the number of files specified
@@ -20,17 +20,17 @@ creates new text file and writes each character in list to this text file.
 Finally subtracts the time and prints time taken for whole execution'''
 start_time = time.time()
 for x in range(numberofFiles):
-    filename = str(x+1)+'karishma.txt'
-    #print filename
+    filename = str(x+1)+'testfile.txt'
+    
     f = open(filename,'w')
     for item in a:
         f.write(item)
     f.close()
-    size = os.stat(filename).st_size  #Change this path to
-    #size = os.stat('C:\\Users\\Prashanth\\Desktop\\Python\\'+filename).st_size  #Change this path to
-    print "Size of file",x+1," written is",size/1000,"KB" # get size of this file
+    size = os.stat(filename).st_size  
+    
+    print ("Size of file",x+1," written is",size/1000,"KB") # get size of this file
 elapsed_time = time.time() - start_time
-print "Total Time to write",elapsed_time,"seconds."
+print ("Total Time to write",elapsed_time,"seconds.")
 
 
 
